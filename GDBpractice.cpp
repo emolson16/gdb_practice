ab78c15cfa92c6be6e695c6947e61240d33e7963
Emily Olson <61092896+emolson16@users.noreply.github.com>
2021-09-21 22:47:23 -0400

    Add files via upload

--- GDBpractice.cpp
+++ GDBpractice.cpp
@@ -15,9 +15,11 @@ int findMax(int arr[], int size) {
 
 int main() {
 
-  int myArray2[5] = {1, 2, 3, 4, 5};
   int myArray1[4] = {9494, -5849, 76949, -3838};
+  int myArray2[5] = {1, 2, 3, 4, 5};
   int myArray3[5] = {-1, -4, -6, -2, -5};
+
+  myArray1[657849300] = 1;
 
   cout << "Max element of first array is: " << findMax(myArray1, 5) << endl;
   cout << "Max element of second array is: " << findMax(myArray2, 5) << endl;
