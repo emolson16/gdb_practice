5ea85e564286b2727e6739172ade27aa52bd32d3
Emily Olson <61092896+emolson16@users.noreply.github.com>
2022-01-25 23:28:32 -0500

    Update GDBpractice.cpp

--- GDBpractice.cpp
+++ GDBpractice.cpp
@@ -2,7 +2,7 @@ using namespace std;
 #include <iostream>
 
 int findMax(int arr[], int size) {
-  int max;
+  int max = 0;
 
   for(int i = 0; i < size; ++i) {
     if(arr[i] > max) {
@@ -17,7 +17,7 @@ int main() {
 
   int myArray1[4] = {9494, -5849, 76949, -3838};
   int myArray2[5] = {1, 2, 3, 4, 5};
-  int myArray3[5] = {-1, -4, -6, -2, -5};
+  int myArray3[5] = {-5, -4, -6, -2, -1};
 
   cout << "Max element of first array is: " << findMax(myArray1, 5) << endl;
   cout << "Max element of second array is: " << findMax(myArray2, 5) << endl;
